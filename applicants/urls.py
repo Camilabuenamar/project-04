5fe0a5b376fc6e922d4a3611269975e009a579a9
camilabuenamar <camila.buenamar@gmail.com>
2019-09-06 10:47:36 +0100

    deleted applicant and companies apps

--- applicants/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ApplicantList, ApplicantDetail
-
-urlpatterns = [
-    path('applicants/', ApplicantList.as_view()),
-    path('applicants/<int:pk>/', ApplicantDetail.as_view()),
-]
--- companies/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CompanyList, CompanyDetail
-
-urlpatterns = [
-    path('companies/', CompanyList.as_view()),
-    path('companies/<int:pk>/', CompanyDetail.as_view()),
-]
